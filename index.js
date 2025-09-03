07dfe50e06f336d691e6fedf40865d9c1838b3d4
yourdiebronze7 <sidnyjernov@hotmail.com>
2025-09-03 09:59:48 -0700

    feat: initial commit for index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.send('Chào mừng đến với nền tảng Huy Chương Bất Vĩ Đại!');
+});
+
+app.listen(port, () => {
+    console.log(`Máy chủ đang chạy trên http://localhost:${port}`);
+});
